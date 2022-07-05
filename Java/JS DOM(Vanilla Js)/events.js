a34a09be26613f1d48ac2440c6fcc5bab8dfb090
Angela Konadu Gyarteng <angelagyarteng@gmail.com>
2022-07-05 13:25:06 +0000

    Hostel management

--- Java/JS DOM(Vanilla Js)/events.js
+++ Java/JS DOM(Vanilla Js)/events.js
@@ -21,6 +21,29 @@
 //}
 var form= document.querySelector("#form")
 var itemList=document.querySelector('#items')
+
+itemList.addEventListener("click",removeItem)
+
+function removeItem(e){
+    //console.log(e.target)
+    if(e.target.classList.contains('delete')){
+      //console.log(1)  
+
+      //simple alert to confirm activity 
+      if(confirm("Are you sure of this decision?")){
+        let li = e.target.parentElement;
+        itemList.removeChild(li)
+      }
+
+      }
+}
+console.log(localStorage)
+localStorage.setItem("jwt","jfjdksofdgknzskv")
+localStorage.setItem('userName','Angie')
+
+displayUserName = document.querySelector("#header-title")
+displayUserName.innerText =localStorage.getItem('userName')
+
 //console.log(form)
 form.addEventListener('submit' ,btnClick)
 function btnClick(e){
@@ -28,6 +51,7 @@ function btnClick(e){
     //console.log("you click me")
 console.log(document.querySelector("#userInput").value)
    var userInput = document.querySelector("#userInput").value
+
    //create new list
    var li = document.createElement('li')
    li.className = "list-group-item"
